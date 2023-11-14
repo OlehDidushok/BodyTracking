5b8416c30c776cbdd5ef660a12aa0e9c8553561f
OlehDidushok <didushokov@gmail.com>
2023-11-14 12:23:25 +0200

    Initial Commit

---
+++ BodyTracking/BodyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTrackingApp.swift
+//  BodyTracking
+//
+//  Created by Oleh on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BodyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
